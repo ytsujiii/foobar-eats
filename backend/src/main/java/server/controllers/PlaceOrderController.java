41b4e64bfd060a94bdb0be352accd6b4c24dcb27
ytsujiii <tsujimon850734@gmail.com>
2022-07-01 22:44:47 +0900

    feat: Orderエンドポイント

---
+++ backend/src/main/java/server/controllers/PlaceOrderController.java
@@ -0,0 +1,25 @@
+package server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import com.scalar.db.exception.transaction.TransactionException;
+import java.io.IOException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+public class PlaceOrderController {
+
+    @RequestMapping(value = "/order", method = RequestMethod.POST)
+    public String placeOrder(@RequestBody OrderDto orderDto) throws Exception {
+        try (Repository repository = new Repository()) {
+            return repository.placeOrder(
+                    orderDto.getCustomerId(),
+                    orderDto.getItemIds(),
+                    orderDto.getItemCounts()
+            );
+        }
+    }
+}
