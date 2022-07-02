08f59877ee91a245e36cdf1136ea0e6e3cd7c520
ytsujiii <tsujimon850734@gmail.com>
2022-07-02 22:16:22 +0900

    chore: set theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
+import { ThemeProvider } from "@mui/material";
 import React from "react";
 import "./App.css";
+import THEME from "./theme";
 
 const App = (): React.ReactElement => {
   return (
     <>
-      <div>aaa</div>
+      <ThemeProvider theme={THEME}>aaa</ThemeProvider>
     </>
   );
 };
---
+++ frontend/src/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+
+const THEME = createTheme({
+  palette: {
+    primary: {
+      main: "#ffffff",
+    },
+  },
+});
+
+export default THEME;
