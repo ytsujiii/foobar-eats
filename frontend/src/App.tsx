f7aaab0187ac1d45424174c270fb3b989b5d894b
ytsujiii <tsujimon850734@gmail.com>
2022-07-03 11:05:53 +0900

    feat: 商品詳細画面

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@mui/material";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ItemDetailPage from "./pages/ItemDetailPage";
 import ItemListPage from "./pages/ItemListPage";
 import THEME from "./theme";
 
@@ -11,6 +12,7 @@ const App = (): React.ReactElement => {
         <ThemeProvider theme={THEME}>
           <Routes>
             <Route path="/items" element={<ItemListPage />} />
+            <Route path="/items/:itemId" element={<ItemDetailPage />} />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
---
+++ frontend/src/pages/ItemDetailPage.tsx
@@ -0,0 +1,48 @@
+import AddIcon from "@mui/icons-material/Add";
+import CloseIcon from "@mui/icons-material/Close";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { Button, IconButton, Typography } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./ItemDetailPage.module.scss";
+
+const ItemDetailPage = (): React.ReactElement => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className={styles["cancel-button-wrapper"]}>
+        <IconButton onClick={() => navigate("/items")} className={styles["cancel-button"]}>
+          <CloseIcon />
+        </IconButton>
+      </div>
+      <div className={styles["thumbnail-wrapper"]}>
+        <img className={styles["thumbnail"]} src="/images/pizza1.jpg" alt="" />
+      </div>
+      <div className={styles["abstract"]}>
+        <Typography variant="h5">Fantastic Pizza</Typography>
+        <Typography variant="h6">$8.99</Typography>
+        <Typography className={styles["description"]}>
+          A deluxe traditional Hawaiian pizza loaded with ham, pineapple, and mozzarella cheese.
+        </Typography>
+      </div>
+      <Typography className={styles["heading"]}>Special Instructions</Typography>
+      <div className={styles["count-input-form-group"]}>
+        <IconButton className={styles["count-button"]}>
+          <RemoveIcon />
+        </IconButton>
+        <Typography className={styles["count-value"]}>1</Typography>
+        <IconButton className={styles["count-button"]}>
+          <AddIcon />
+        </IconButton>
+      </div>
+      <div className={styles["footer"]}>
+        <Button onClick={() => navigate("/items")} className={styles["add-to-cart-button"]}>
+          Add to cart・$8.99
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default ItemDetailPage;
