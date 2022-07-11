c0b6c6096d95f471c1e0d943fb010acbd82f64aa
ytsujiii <tsujimon850734@gmail.com>
2022-07-11 21:55:15 +0900

    refactor: 仮の詳細ページを消す

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { ThemeProvider } from "@mui/material";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DeliveryDetailPage from "./pages/DeliveryDetailPage";
-import Item1DetailPage from "./pages/Item1DetailPage";
 import ItemDetailPage from "./pages/ItemDetailPage";
 import ItemListPage from "./pages/ItemListPage";
 import THEME from "./theme";
@@ -14,7 +13,6 @@ const App = (): React.ReactElement => {
         <ThemeProvider theme={THEME}>
           <Routes>
             <Route path="/items" element={<ItemListPage />} />
-            <Route path="/items/1" element={<Item1DetailPage />} />
             <Route path="/items/:itemId" element={<ItemDetailPage />} />
             <Route path="/delivery" element={<DeliveryDetailPage />} />
           </Routes>
