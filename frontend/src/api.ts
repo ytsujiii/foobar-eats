d29db0470dd284944156330ed89c37c0887ed60c
ytsujiii <tsujimon850734@gmail.com>
2022-07-16 14:35:53 +0900

    fix: API base urlのデフォルト値

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -4,7 +4,7 @@ import Item from "./types/Item";
 import Order from "./types/Order";
 
 const client = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080',
 });
 
 export default class Api {
